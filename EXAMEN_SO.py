1d380703bd2b81ac0f518281c4fb01e08bd7db80
CeciliaZL <cecilialeonfi@gmail.com>
2023-04-27 10:46:05 -0600

    Estatica

--- EXAMEN_SO.py
+++ EXAMEN_SO.py
@@ -1,4 +1,8 @@
 #Datos que se usaran para las particiones  
+
+
+import numpy as np
+
 def datos():
      memoriaT = 2048
      memoriaB = 640
@@ -7,30 +11,60 @@ def datos():
 
 def pEstatica():
 
+    valores = []
     print("--------------------------------------")
     print("\n\tPartición estática")
     particion = int(input("\tIngrese el número de partiones que requiere\n\n"))
     memoriaApps =1024
     a = 0 
+    b = []
+    o = 0
+
+########################   No tiene que salir particion final si se sale en el if pasado  #############################################
+
 
     if (particion <=10):
         for i in range(0,particion-1):
-            print("\tPartición ", i +1)
+            print("\n\tPartición ", i +1)
             valPart = int(input("\tTamaño: ")) 
+            valores.append(valPart)
+            #valores.sort()
+
+            
             if (valPart<=memoriaApps):
                 a = a + valPart
-                print("aoaoaao: ", a)        
-                print("La suma es ", a)
-                
+                              
             else:
-                print("El tamaño de la particion es mayo al de la memoria")
+                print("El tamaño de la particion es mayor al de la memoria")
                 break
-            
+        b = memoriaApps - a
+        print("\tPartición ", particion)  
+        print ("\tTamaño: ", b)
+        ##################Puedo meter una suma para hacer una lista anidad<
+        #o = a+b  
+        #print("\nLa suma es ", o)
+        print("El valor de las particiones son ", particion)
+        vFinal = np.append(valores, b)
+        print("El valor de x es ",vFinal)
+
+        for j in range (0, particion ):
+            print("\n\tProceso ", j + 1 )
+            proceso = int(input("\tTamaño: "))
+
     
     else: 
          print("Solicita más de las particiones permitidas")
 
-    
+
+
+###############################################################################################################################################333
+
+
+
+
+
+
+
 
 
 
@@ -39,7 +73,40 @@ def pEstatica():
 
 
 def pDinamica():
-	print('dinamico')
+
+    print("--------------------------------------")
+    print("\n\tPartición Dinámica")
+    particion = int(input("\tIngrese el número de partiones que requiere\n\n"))
+    memoriaApps =1024
+    procesos = 0
+    a = 0 
+    b = 0
+
+    if (particion <=10):
+        for i in range(0,particion-1):
+            print("\tPartición ", i +1)
+            valPart = int(input("\tTamaño: ")) 
+            if (valPart<=memoriaApps):
+                a = a + valPart
+                              
+            else:
+                print("El tamaño de la particion es mayo al de la memoria")
+                break
+        b = memoriaApps - a
+        print("Partición ", 1 + i, "es: ",b)   
+        print("La suma es ", a)             
+    
+    else: 
+         print("Solicita más de las particiones permitidas")
+
+    procesos = particion
+    for p in range (0, procesos):
+        procesos = int(input("\n\n\tIngres el proceso ", p + 1))
+        #if (procesos <= valPart):
+
+         
+
+
 
 def pPaginacion():
 	print('paginacion')
